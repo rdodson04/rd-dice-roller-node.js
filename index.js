79280766a09112cf6432e4f9b97c74f88c4b3a6f
Your Name <ryandodson@lewisu.edu>
2024-03-01 01:44:00 -0600

    dice rolls

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+app = express()
 
 const cors = require("cors")
-const port = 3000;
 
 var url = require('url');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 3
 
+// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: '*' }))
 
+
+app.get('/version', (request, response) => {
+	console.log('Calling "/version" on the Node.js server.')
+	response.type('text/plain')
+	response.send('Version: '+majorVersion+'.'+minorVersion)
+})
+
+app.get('/api/ping', (request, response) => {
+	console.log('Calling "/api/ping"')
+	response.type('text/plain')
+	response.send('ping response')
+})
 
 
 app.get('/roll-dice', (req, res) => {
@@ -28,6 +33,23 @@ app.get('/roll-dice', (req, res) => {
   res.json({ result });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
+// Custom 404 page.
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(404)
+  response.send('404 - Not Found')
+})
+
+// Custom 500 page.
+app.use((err, request, response, next) => {
+  console.error(err.message)
+  response.type('text/plain')
+  response.status(500)
+  response.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started at \"http://localhost:${port}\"\n` +
+  `press Ctrl-C to terminate.`)
+)
